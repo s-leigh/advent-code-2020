b1d35aeb48516cc518d11283618c9fd70366c840
Stephen Leigh <8478517+s-leigh@users.noreply.github.com>
2024-02-14 09:30:12 +0000

    Enhance createDay script

--- createDay.sh
+++ createDay.sh
@@ -4,5 +4,43 @@ day=$1
 if [ -z ${day} ]; then echo "Must provide day" && exit 1; fi
 
 touch input/day${day}Input
-touch src/day${day}.ts
-touch test/day${day}.test.ts
+
+fileInit=$'
+
+export const day'${day}'Part01 = (input: string): number => {
+
+}
+
+export const day'${day}'Part02 = (input: string): number => {
+  return -1
+}
+'
+
+testInit=$'import { describe, test } from "node:test"
+import assert from "node:assert"
+import { day01Part01, day01Part02 } from "../src/day01"
+import fs from "fs"
+
+const SAMPLE_INPUT = ``
+
+describe("Day '"${day}"\"', () => {
+  test("Day '${day}' part 01 sample input", () => {
+    
+  })
+
+  test("Day '${day}' part 01", () => {
+
+  })
+
+//  test("Day '${day}' part 02 sample input", () => {
+//    
+//  })
+//
+//  test("Day '${day}' part 02", () => {
+//    
+//  })
+})
+'
+
+echo "$fileInit" > src/day${day}.ts
+echo "$testInit" > test/day${day}.test.ts
